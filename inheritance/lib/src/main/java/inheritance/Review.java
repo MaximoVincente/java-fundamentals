7a02f27453bb57bb73bc0a50f6b89a6425345e00
Maximo Vincente Mejia <maximovincente@gmail.com>
2022-10-31 20:47:35 -0400

    Finished 1-3

---
+++ inheritance/lib/src/main/java/inheritance/Review.java
@@ -0,0 +1,48 @@
+package inheritance;
+
+public class Review  {
+
+    private String body;
+    private double starRating;
+    private String author;
+
+
+
+    public Review(double starRating, String body, String author) {
+
+        this.body = body;
+        this.starRating = starRating;
+        this.author = author;
+
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public double getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(double starRating) {
+        this.starRating = starRating;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+
+    @Override
+    public String toString() {
+        return   "Review: " + getBody() + "\n" + "Rating: " + getStarRating() + "\n" + " Author: " + getAuthor();
+    }
+}
